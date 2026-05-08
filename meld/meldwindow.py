097f18f735808994b4047f974700b2615343d745
Kai Willadsen <kai.willadsen@gmail.com>
2026-05-08 14:37:17 +1000

    Ensure that VC state is available in single-file open cases (#486)
    Because of how we instantiate the single-file VC open, the actual VC
    state refresh doesn't get run by the time we call `run_diff()`, and so
    we get the default state (NORMAL) for the provided path. The effect of
    this is that conflicted files are opened as a two-pane comparison
    instead of in a three-way conflict resolution mode.
    
    Ideally we'd disentangle the VC logic a bit here, but that's quite
    complicated and honestly I really want to rewrite most of it to use
    libgit2 instead, so this is the shortcut fix.

--- meld/meldwindow.py
+++ meld/meldwindow.py
@@ -466,6 +466,8 @@ class MeldWindow(Gtk.ApplicationWindow):
         self.scheduler.add_scheduler(doc.scheduler)
         path = gfile.get_path()
         doc.set_location(path)
+        # Ensure that we have the correct state for the file we're opening
+        doc.vc.refresh_vc_state()
         doc.create_diff_signal.connect(
             lambda obj, arg, kwargs: self.append_diff(arg, **kwargs))
         doc.run_diff(path)
